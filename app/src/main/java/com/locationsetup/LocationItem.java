ac64d71a759d42d0f939544e974713b272befe97
DongHeeYun <inter0960@naver.com>
2017-11-02 18:38:28 +0900

    LocationItem 변경

--- app/src/main/java/com/locationsetup/LocationItem.java
+++ app/src/main/java/com/locationsetup/LocationItem.java
@@ -74,6 +74,38 @@ public class LocationItem {
             data=0;
     }
 
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
     public int getWifi() {
         return wifi;
     }
