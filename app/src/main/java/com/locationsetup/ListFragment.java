f874432b4b38f9de4dee6a7335c52b0547d2cc7d
inter <inter0960@naver.com>
2017-11-07 04:54:57 +0900

    리사이클러뷰를 이용해 ListFragment 내용 변경

--- app/src/main/java/com/locationsetup/ListFragment.java
+++ app/src/main/java/com/locationsetup/ListFragment.java
@@ -3,9 +3,16 @@ package com.locationsetup;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,13 +20,15 @@ import android.view.ViewGroup;
  */
 public class ListFragment extends Fragment {
 
+    View view;
+    static public ListFragment fragment = new ListFragment();
 
     public ListFragment() {
         // Required empty public constructor
     }
 
     public static ListFragment newInstance() {
-        return new ListFragment();
+        return fragment;
     }
 
 
@@ -27,7 +36,27 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        view = inflater.inflate(R.layout.fragment_list, container, false);
+        tempCreate(view);
+        return view;
+    }
+
+    private void tempCreate(View view){
+        LocationItem locationItem = new LocationItem("house","address");
+        ArrayList<LocationItem> locationItems = new ArrayList<LocationItem>();
+        locationItems.add(new LocationItem("school","address"));
+
+        RecyclerView reView = (RecyclerView) view.findViewById(R.id.reView);
+
+        locationItems.add(locationItem);
+
+        ListAdapter adapter = new ListAdapter(view.getContext(),locationItems);
+        reView.setAdapter(adapter);
+
+        StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+        reView.setLayoutManager(gridLayoutManager);
+
+        adapter.notifyDataSetChanged();
     }
 
 }
